8ec3c859e1a8b6901b260e6e4afdeed33b61bab8
Geoffroy MAIDOU <maidougeoffroyjaphet@gmail.com>
2022-03-17 23:56:53 +0000

    password_edit

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -3,26 +3,54 @@
 namespace App\Controller;
 
 use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountPasswordController extends AbstractController
 {
-    #[Route('/compte/modifier-password', name: 'app_account_password')]
-    public function index(Request $request): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        // $user = $this->getUser();
-        // $form = $this->createForm(ChangePasswordType::class, $user);
+        $this->entityManager = $entityManager;
+    }
+    #[Route('/compte/modifier-password', name: 'app_account_password')]
+    public function index(Request $request,UserPasswordHasherInterface $hasher): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
 
-        // $form->handleRequest($request);
+        $form->handleRequest($request);
 
-        // if ($form->isSubmitted() && $form->) {
-        //     # code...
-        // }
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
 
-        return $this->render('account/password.html.twig');
+            $old_pwd = $form->get('old_password')->getData();
+      
+            if($hasher->isPasswordValid($user, $old_pwd))
+                {
+                   
+                    $new_pwd = $form->get('new_password')->getData();
+                    $password = $hasher->hashPassword($user, $new_pwd);
+                    $user->setPassword($password);
+
+
+                    $this->entityManager->persist($user);
+                    $this->entityManager->flush();
+                }
+            
+
+          
+        }
+
+        return $this->render('account/password.html.twig',[
+            'form'=> $form->createView()
+        ]);
     }
 }
